5f383361b7d245dd35390d5bfcd1a2c6cdfc1cd9
vladis18 <vladisbabayan@gmail.com>
2021-12-16 15:36:36 -0500

    The sxi PUffy

--- src/test/java/practice/Jenkins.java
+++ src/test/java/practice/Jenkins.java
@@ -3,7 +3,7 @@ package practice;
 public class Jenkins {
     public static void main(String[] args) {
         for(int i=0;i<=5;i++){
-            System.out.println("I love you so so  so much");
+            System.out.println("I love you so so  so s0 much");
         }
     }
 }
