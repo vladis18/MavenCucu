bd123864f501d6e4bcec25bdf8ed0a5d7ef5ca02
vladis18 <vladisbabayan@gmail.com>
2021-12-16 15:18:17 -0500

    Puffy the 3rd

--- src/test/java/practice/Jenkins.java
+++ src/test/java/practice/Jenkins.java
@@ -3,7 +3,7 @@ package practice;
 public class Jenkins {
     public static void main(String[] args) {
         for(int i=0;i<=5;i++){
-            System.out.println("I love you");
+            System.out.println("I love you so much");
         }
     }
 }
